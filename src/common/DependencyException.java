c66c59a55ddf3429235e66d0afdff95cd51cf685
Xavier <xaavier@protonmail.com>
2020-05-06 00:30:55 +0200

    Initial structure

---
+++ src/common/DependencyException.java
@@ -0,0 +1,10 @@
+package common;
+
+public class DependencyException extends Exception {
+    public DependencyException(Exception cause) {
+        super(cause);
+    }
+    public DependencyException(String message) {
+        super(message);
+    }
+}
---
+++ src/complex/Container.java
@@ -0,0 +1,6 @@
+package complex;
+
+public class Container extends Factory {
+    Factory factory = new Factory();
+
+}
